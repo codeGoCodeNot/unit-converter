f7d7cb24f23913a2bf3d0913f772f945c4538541
sendosend <johnsenberdin2930@gmail.com>
2025-08-19 13:40:50 +0800

    UX done

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const toggleTheme = () => {
   const main = document.querySelector('main');
   const toggleBtn = document.querySelector('.toggle-btn');
+  const unitConverters = document.querySelectorAll('.unit-converter');
+  const ps = document.querySelectorAll('p');
   toggleBtn.addEventListener('click', () => {
     main.classList.toggle('dark');
-    toggleBtn.classList.toggle('dark');
-
-    toggleBtn.classList.contains('dark');
 
     main.classList.contains('dark')
       ? (toggleBtn.textContent = 'Light mode')
       : (toggleBtn.textContent = 'Dark mode');
+
+    unitConverters.forEach((box) => {
+      box.classList.toggle('dark');
+      box.classList.toggle('light');
+
+      ps.forEach((p) => {
+        p.classList.toggle('dark');
+        p.classList.toggle('light');
+      });
+    });
   });
 };
 
-toggleTheme();
+const init = () => {
+  toggleTheme();
+};
+
+init();
